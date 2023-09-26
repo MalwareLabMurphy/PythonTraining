b2ff598e4a5371d85f5bd509a17922a78ede0028
malwarelabmurphy <malwarelabmurphy@gmail.com>
2023-09-26 16:24:23 -0400

    First lesson on basic syntax.

---
+++ Basic_Syntax/Basic_Syntax.py
@@ -0,0 +1,30 @@
+##NB: Remove hashtag/octothorpe from beginning of a given command in order to execute.
+
+##Variable and if conditions
+
+#x=5
+#if x > 4:
+#    print("Greater!")
+
+##Case sensitivity and variables
+
+#variable= "This is a variable"
+#Variable= "This is a different variable"
+#print(variable)
+#print(Variable)
+#print("variable")
+
+##Types of values
+
+#type("string")
+#type(99)
+#type(True)
+#type(99.9)
+
+##F-strings
+##Reminder: integers cannot be concatenated with strings!
+
+#name="Murphy"
+#breed="Labrador"
+#age=7
+#print("Hello! My name is " + name + " the " + breed + " and I am " + str(age) + " years old!")
