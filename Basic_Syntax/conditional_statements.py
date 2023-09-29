b314385f5d3f4566a0aecbee292d80e57b152e54
malwarelabmurphy <malwarelabmurphy@gmail.com>
2023-09-28 21:40:21 -0400

    Second lesson, renamed files.

---
+++ Basic_Syntax/conditional_statements.py
@@ -0,0 +1,13 @@
+##Order: IF > ELIF > ELSE
+
+##Age verification
+
+age=7
+if age > 60:
+    print("You are a senior")
+elif age > 21:
+    print("You can drink!")
+elif age > 18:
+    print("You're kind of an adult?")
+else:
+    print("You're a minor!")
